2b9b181228de238d891e0836cc8c310c2350532f
Sasha Ajintarev <77781886+sasha-ajin@users.noreply.github.com>
2021-07-06 14:33:09 +0300

    Update forms.py

--- store/forms.py
+++ store/forms.py
@@ -1,6 +1,12 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, Form,CharField,PasswordInput
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+
+
+class LoginUserForm(Form):
+    username = CharField(label='username', max_length=100)
+    password = CharField(widget=PasswordInput())
+
 
 
 class CreateUserForm(UserCreationForm):
